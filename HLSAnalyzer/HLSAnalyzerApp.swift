52a66ca4f67d91ba78dfb62d8bdbd80c3729a539
Benjamin Lavialle <benjamin.lavialle@fabernovel.com>
2022-05-27 11:08:48 +0200

    Initial Commit

---
+++ HLSAnalyzer/HLSAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HLSAnalyzerApp.swift
+//  HLSAnalyzer
+//
+//  Created by Benjamin Lavialle on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct HLSAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
