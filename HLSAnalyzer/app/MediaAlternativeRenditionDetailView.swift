b5bb43f9f50488f7b95436b219740d1af82db7da
Benjamin Lavialle <benjamin.lavialle@fabernovel.com>
2022-06-03 15:27:20 +0200

    Add content to alternative rendition view

--- HLSAnalyzer/app/MediaAlternativeRenditionDetailView.swift
+++ HLSAnalyzer/app/MediaAlternativeRenditionDetailView.swift
@@ -13,6 +13,16 @@ struct MediaAlternativeRenditionDetailView: View {
     let mediaAlternativeRendition: MediaAlternativeRendition
 
     var body: some View {
-        Text("DETAIL !!!")
+        VStack(spacing: 8) {
+            KeyValueView(key: "alternative_rendition_type", value: "\(mediaAlternativeRendition.type)")
+            KeyValueView(key: "alternative_rendition_group", value: mediaAlternativeRendition.group)
+            KeyValueView(key: "alternative_rendition_name", value: mediaAlternativeRendition.name)
+            KeyValueView(key: "alternative_rendition_language", value: mediaAlternativeRendition.language)
+            KeyValueView(key: "alternative_rendition_URI", value: mediaAlternativeRendition.URI.flatMap { "\($0)" } ?? "_")
+            KeyValueView(key: "alternative_rendition_defaultRendition", value: "\(mediaAlternativeRendition.defaultRendition)")
+            KeyValueView(key: "alternative_rendition_autoselect", value: "\(mediaAlternativeRendition.autoselect)")
+            Spacer()
+        }
+        .padding(20)
     }
 }
